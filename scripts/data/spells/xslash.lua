452f120b4750e4241b05b69befbca70c76ca07ae
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-11-04 00:02:57 -0500

    XSlashSpell object

--- scripts/data/spells/xslash.lua
+++ scripts/data/spells/xslash.lua
@@ -30,31 +30,9 @@ end
 function spell:onCast(user, target)
 	local damage = math.floor((((user.chara:getStat("attack") * 150) / 20) - 3 * (target.defense)) * 1.3)
 
-	local function generateSlash(scale_x)
-		local cutAnim = Sprite("effects/attack/cut")
-		Assets.playSound("scytheburst")
-		Assets.playSound("criticalswing", 1.2, 1.3)
-		user.overlay_sprite:setAnimation("battle/attack") -- Makes the afterimages use the first frame of the attack animation
-		user:toggleOverlay(true)
-		local afterimage1 = AfterImage(user, 0.5)
-		local afterimage2 = AfterImage(user, 0.6)
-		user:toggleOverlay(false)
-		afterimage1.physics.speed_x = 2.5
-		afterimage2.physics.speed_x = 5
-		afterimage2:setLayer(afterimage1.layer - 1)
-		user:setAnimation("battle/attack", function()
-			user:setAnimation("battle/idle")
-		end)
-		user:flash()
-		cutAnim:setOrigin(0.5, 0.5)
-		cutAnim:setScale(2.5 * scale_x, 2.5)
-		cutAnim:setPosition(target:getRelativePos(target.width/2, target.height/2))
-		cutAnim.layer = target.layer + 0.01
-		cutAnim:play(1/15, false, function(s) s:remove() end)
-		user.parent:addChild(cutAnim)
-		user.parent:addChild(afterimage1)
-		user.parent:addChild(afterimage2)
-	end
+	---@type XSlashSpell
+	local spellobj = XSlashSpell(user,target)
+	Game.battle:addChild(spellobj):setLayer(BATTLE_LAYERS["above_battlers"])
 
 	local function dealDamage()
 		local strikedmg = damage
@@ -66,9 +44,9 @@ function spell:onCast(user, target)
 	end
 	Game.battle.timer:after(0.1/2, function()
 		dealDamage()
-		generateSlash(1)
+		spellobj:generateSlash(1)
 		Game.battle.timer:after(1/2, function()
-			generateSlash(-1)
+			spellobj:generateSlash(-1)
 			dealDamage()
 		end)
 	end)
--- scripts/objects/XSlashSpell.lua
+++ scripts/objects/XSlashSpell.lua
@@ -2,4 +2,36 @@
 ---@overload fun(...): XSlashSpell
 local XSlashSpell, super = Class(Object)
 
+function XSlashSpell:init(user,target)
+    super.init(self)
+    self.user = user
+    self.target = target
+end
+
+function XSlashSpell:generateSlash(scale_x)
+    local cutAnim = Sprite("effects/attack/cut")
+    Assets.playSound("scytheburst")
+    Assets.playSound("criticalswing", 1.2, 1.3)
+    self.user.overlay_sprite:setAnimation("battle/attack") -- Makes the afterimages use the first frame of the attack animation
+    self.user:toggleOverlay(true)
+    local afterimage1 = AfterImage(self.user, 0.5)
+    local afterimage2 = AfterImage(self.user, 0.6)
+    self.user:toggleOverlay(false)
+    afterimage1.physics.speed_x = 2.5
+    afterimage2.physics.speed_x = 5
+    afterimage2:setLayer(afterimage1.layer - 1)
+    self.user:setAnimation("battle/attack", function()
+        self.user:setAnimation("battle/idle")
+    end)
+    self.user:flash()
+    cutAnim:setOrigin(0.5, 0.5)
+    cutAnim:setScale(2.5 * scale_x, 2.5)
+    cutAnim:setPosition(self.target:getRelativePos(self.target.width/2, self.target.height/2))
+    cutAnim.layer = self.target.layer + 0.01
+    cutAnim:play(1/15, false, function(s) s:remove() end)
+    self:addChild(cutAnim)
+    self:addChild(afterimage1)
+    self:addChild(afterimage2)
+end
+
 return XSlashSpell
